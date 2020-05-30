bb41dc908beb0abef4edbdf2407649765a5eb05c
memon07 <memonshoyeb26@gmail.com>
2020-05-30 22:06:26 +0530

    changes in linting

--- app/screens/monthly/index.tsx
+++ app/screens/monthly/index.tsx
@@ -8,14 +8,10 @@ type Props = RouteComponentProps;
 const MonthlyView = ({ history }: Props) => {
   return (
     <div>
-      <h2 className="m-5">Monthly View</h2>
-      <button type="button" className="btn btn-primary" onClick={() => history.push(routes.WEEKLY)}>
+      <h2>Monthly View</h2>
+      <button type="button" onClick={() => history.push(routes.WEEKLY)}>
         go to weekly
       </button>
-      <div className="row">
-        <div className="col-6">aaa</div>
-        <div className="col-6">aaaa</div>
-      </div>
     </div>
   );
 };
