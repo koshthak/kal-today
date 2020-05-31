6973e84ca2172c8c6dcb87b09096d995e001fb00
memon07 <memonshoyeb26@gmail.com>
2020-05-31 18:41:20 +0530

    i18n

--- app/screens/monthly/index.tsx
+++ app/screens/monthly/index.tsx
@@ -2,15 +2,30 @@ import React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 
 import routes from '../../constants/routes.json';
+import { useTranslation } from 'react-i18next';
+import i18next from 'i18next';
 
 type Props = RouteComponentProps;
 
-const MonthlyView = ({ history }: Props) => {
+function MonthlyView({ history }: Props) {
+  const { t } = useTranslation();
+  
+  function handleClick(lang:string) {
+    i18next.changeLanguage(lang)
+  }
+
   return (
     <div>
       <h2>Monthly View</h2>
+      <button onClick={()=>handleClick('US')} >
+            English
+      </button>
+      <button onClick={()=>handleClick('SA')} >
+            Korean
+      </button>
       <button type="button" onClick={() => history.push(routes.WEEKLY)}>
-        go to weekly
+        go to weekly 
+        <h3>{t('Close.1')}</h3>
       </button>
     </div>
   );
