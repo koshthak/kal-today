a66bb980983ad4bfee378815e2a28dcdfd0cf91b
yadav-saurabh <yadav-saurabh@outlook.com>
2020-06-17 09:57:57 +0530

    added minHeight and minWidth

--- app/main.dev.ts
+++ app/main.dev.ts
@@ -55,8 +55,8 @@ const createWindow = async () => {
 
   mainWindow = new BrowserWindow({
     show: false,
-    width: 1024,
-    height: 728,
+    minWidth: 1600,
+    minHeight: 1000,
     webPreferences:
       process.env.NODE_ENV === 'development' || process.env.E2E_BUILD === 'true'
         ? {
