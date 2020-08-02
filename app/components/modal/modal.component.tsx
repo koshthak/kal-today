86b88bca2dca3878437450f04812d2d13b4121c5
memon07 <memonshoyeb26@gmail.com>
2020-08-02 20:36:28 +0530

    added input field

--- app/components/modal/modal.component.tsx
+++ app/components/modal/modal.component.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactModal from 'react-modal';
 
-ReactModal.setAppElement('#modal');
+import closeImg from '../../../internals/img/close.svg';
+
+// ReactModal.setAppElement('#modal');
 
 type Props = {
   title: string;
@@ -18,19 +20,37 @@ const customStyles = {
     right: 'auto',
     bottom: 'auto',
     marginRight: '-50%',
-    transform: 'translate(-50%, -50%)'
+    height: '70%',
+    width: '70%',
+    transform: 'translate(-50%, -50%)',
+    // border: '2px solid black', 
   }
 };
 
+const closeBtn = {
+  float: 'right',
+  height: 20,
+}
+
+const childMainDiv = {
+  marginTop: 20,
+}
+
 const Modal: React.FC<Props> = ({ children, ...props }: Props) => {
+  const { closeModal } = props;
   return (
     <ReactModal
-      // ariaHideApp={false}
+      ariaHideApp={false}
       style={customStyles}
       // eslint-disable-next-line react/jsx-props-no-spreading
       {...props}
     >
-      {children}
+      <div>
+        <img src={closeImg} style={closeBtn} onClick={closeModal} alt="close-btn"/>
+      </div>
+      <div style={childMainDiv}>
+        {children}
+      </div>
     </ReactModal>
   );
 };
--- app/components/sidebar/event.sidebar.tsx
+++ app/components/sidebar/event.sidebar.tsx
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
 
 import Modal from '../modal/modal.component';
+import addImg from '../../../internals/img/plus-white.svg';
+
+const openBtn = {
+  height: 30,
+  cursor:'pointer',
+}
 
 const Event: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -16,11 +22,22 @@ const Event: React.FC = () => {
     <>
       <div>
         <Modal title="Add New Event" isOpen={isOpen} closeModal={closeModal}>
-          <div>hi</div>
+          <div className="row m-0">
+            <label className="p-0">Title</label>
+            <div className="mainInputDiv">
+              <input className="effect1" type="text" placeholder="Enter title" />
+              <span className="focusbg"></span>
+            </div>
+
+            <label className="mt-3 p-0">Description</label>
+            <div className="mainInputDiv">
+              <textarea rows="5" className="effect1" type="text" placeholder="Enter description" />
+              <span className="focusbg"></span>
+            </div>
+
+          </div>
         </Modal>
-        <button type="button" onClick={openModal}>
-          open Event
-        </button>
+        <img src={addImg} style={openBtn} onClick={openModal} alt="open-btn"/>
       </div>
     </>
   );
