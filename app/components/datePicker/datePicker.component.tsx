ab3cb176ccfdd6ae332db85f839485a9856b49b0
memon07 <memonshoyeb26@gmail.com>
2020-09-03 21:13:55 +0530

    added date-range

--- app/components/datePicker/datePicker.component.tsx
+++ app/components/datePicker/datePicker.component.tsx
@@ -1,20 +1,27 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { DateRangePicker } from 'react-date-range';
 import { addDays } from 'date-fns';
 
-const [state, setState] = useState([
-  {
-    startDate: new Date(),
-    endDate: addDays(new Date(), 7),
-    key: 'selection'
-  }
-]);
 
-<DateRangePicker
-  onChange={item => setState([item.selection])}
-  showSelectionPreview={true}
-  moveRangeOnFirstSelection={false}
-  months={2}
-  ranges={state}
-  direction="horizontal"
-/>;
+const DatePicker: React.FC = () => {
+    const [state, setState] = useState([
+        {
+          startDate: new Date(),
+          endDate: addDays(new Date(), 7),
+          key: 'selection'
+        }
+      ]);
+           
+    return (
+        <DateRangePicker
+            onChange={item => {setState([item.selection]),console.log(item)}}
+            showSelectionPreview={true}
+            moveRangeOnFirstSelection={false}
+            months={1}
+            ranges={state}
+            direction="horizontal"
+        />
+    );
+  };
+  
+  export default DatePicker;
