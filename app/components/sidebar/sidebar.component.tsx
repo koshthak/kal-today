dcdc7d0ce3f99176dfb905c48bb834ef0d630ce9
memon07 <memonshoyeb26@gmail.com>
2020-09-06 23:36:12 +0530

    added translation

--- app/components/sidebar/sidebar.component.tsx
+++ app/components/sidebar/sidebar.component.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import i18next from 'i18next';
+import { useTranslation } from 'react-i18next';
 
 import SidebarHeader from './header.sidebar';
 import SidebarDates from './dates.sidebar';
@@ -8,6 +9,7 @@ import Event from './event.sidebar';
 import styles from './sidebar.scss';
 
 const Sidebar: React.FC = () => {
+  const { t } = useTranslation();
   const handleClick = (lang: string) => {
     i18next.changeLanguage(lang);
   };
@@ -17,13 +19,13 @@ const Sidebar: React.FC = () => {
       <SidebarHeader />
       <SidebarDates />
       <div className={styles.event}>
-        <div className={styles.eventTitle}>Add a new Event</div>
+        <div className={styles.eventTitle}>{t('newEvent')}</div>
         <div className={styles.modalDiv}>
           <Event />
         </div>
       </div>
       <div style={{ position: 'absolute', bottom: '5px', left: '100px' }}>
-        <p style={{textAlign:'center'}}>Change Language</p>
+        <p style={{textAlign:'center'}}>{t('changeLanguage')}</p>
         <button type="button" className="btn-line btn-line-square btn-line-sm btn-line-ghost-primary" onClick={() => handleClick('en')}>
           English
         </button>
