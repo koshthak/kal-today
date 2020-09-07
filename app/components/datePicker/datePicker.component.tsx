402903b3a8f911478be007dabad0053a707706b5
memon07 <memonshoyeb26@gmail.com>
2020-09-07 15:28:09 +0530

    making event form

--- app/components/datePicker/datePicker.component.tsx
+++ app/components/datePicker/datePicker.component.tsx
@@ -3,7 +3,7 @@ import { DateRangePicker } from 'react-date-range';
 import { addDays } from 'date-fns';
 
 
-const DatePicker: React.FC = () => {
+const DatePicker = (props: { setRange: Function }) => {
     const [state, setState] = useState([
         {
           startDate: new Date(),
@@ -11,10 +11,17 @@ const DatePicker: React.FC = () => {
           key: 'selection'
         }
       ]);
+
+    const onDateChange = (item: any) => {
+      setState([item.selection]);
+      props.setRange(item.selection);
+      console.log(item);
+    }
            
     return (
         <DateRangePicker
-            onChange={item => {setState([item.selection]),console.log(item)}}
+            // onChange={item => {setState([item.selection]),console.log(item)}}
+            onChange={onDateChange}
             showSelectionPreview={true}
             moveRangeOnFirstSelection={false}
             color="#6f52ed"
--- app/components/sidebar/event.sidebar.tsx
+++ app/components/sidebar/event.sidebar.tsx
@@ -3,9 +3,40 @@ import React from 'react';
 import Modal from '../modal/modal.component';
 import { useTranslation } from 'react-i18next';
 import DatePicker from '../datePicker/datePicker.component';
+import { addDays } from 'date-fns';
+
+interface HandleInputChangeInterface {
+  target: HTMLInputElement;
+}
+
+interface HandleTextAreaChangeInterface {
+  target: HTMLTextAreaElement;
+}
 
 const Event = (props: { isOpen: boolean, closeModal: () => void }) => {
   const { t } = useTranslation();
+
+  const [title, setTitle] = React.useState('');
+  const [description, setDescription] = React.useState('');
+  const [range, setRange] = React.useState({
+    startDate: new Date(),
+    endDate: addDays(new Date(), 7),
+    key: 'selection'
+  });
+
+  const onSetTitle = (e: HandleInputChangeInterface) => {
+    setTitle(e.target.value.trim())
+    // console.log(e.target.value)
+  }
+
+  const onSetDescription = (e: HandleTextAreaChangeInterface) =>{
+    setDescription(e.target.value.trim())
+    // console.log(e.target.value)
+  }
+
+  const submitEvent = () => {
+    console.log(title,description,range);
+  }
 
   return (
     <>
@@ -17,6 +48,8 @@ const Event = (props: { isOpen: boolean, closeModal: () => void }) => {
               <input
                 className="effect1"
                 type="text"
+                value={title}
+                onChange={onSetTitle}
                 placeholder="Enter title"
               />
               <span className="focusbg" />
@@ -27,6 +60,8 @@ const Event = (props: { isOpen: boolean, closeModal: () => void }) => {
               <textarea
                 rows={5}
                 className="effect1"
+                value={description}
+                onChange={onSetDescription}
                 placeholder="Enter description"
               />
               <span className="focusbg" />
@@ -34,12 +69,14 @@ const Event = (props: { isOpen: boolean, closeModal: () => void }) => {
 
             <label className="mt-3 p-0">{t('chooseDate')}</label>
             <div className="mainInputDiv">
-              <DatePicker/>
+              <DatePicker setRange={setRange} />
               <span className="focusbg" />
             </div>
 
             <div className="pt-3">
-              <button type="button" className="btn-line-inverse btn-line-square btn-line-sm btn-line-ghost-inverse-primary">
+              <button type="button" 
+                onClick={submitEvent}
+                className="btn-line-inverse btn-line-square btn-line-sm btn-line-ghost-inverse-primary">
                 {t('create')}
               </button>
             </div>
